dcdb45505880dc4da17f5fec78499df6b560b7ad
satishgangane90 <satishgangane@gmail.com>
2024-09-16 17:46:42 +0530

    Task13092024

---
+++ src/sept/Task130924/SimpleCalculator.java
@@ -0,0 +1,34 @@
+package sept.Task130924;
+
+public class SimpleCalculator {
+
+    public  static void main(String[] args) {
+
+
+        double a = 34;
+        double b = 10;
+
+        double add= a+b;
+        double sub= a-b;
+        double multi= a*b;
+        double div= a/b;
+        double rem= a%b;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+        System.out.println(rem);
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ src/sept/Task130924/TableOfNumber.java
@@ -0,0 +1,24 @@
+package sept.Task130924;
+
+public class TableOfNumber {
+
+    public  static void main(String[] args) {
+
+        int num = 10;
+        for (int i=1; i<=10; i++) {
+
+            int fin = num*i;
+
+            System.out.println(num + " * " + i + " = " + fin);
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
