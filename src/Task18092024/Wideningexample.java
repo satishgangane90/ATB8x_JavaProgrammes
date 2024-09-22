4856f247c1e87deff3538f626bc6ac99617c4411
satishgangane90 <satishgangane@gmail.com>
2024-09-22 20:54:28 +0530

    Task13092024

---
+++ src/Task18092024/Wideningexample.java
@@ -0,0 +1,22 @@
+package Task18092024;
+
+public class Wideningexample {
+
+    public static void main(String[] args) {
+
+        int baseprice = 500;
+
+        double taxrate = 0.18;
+
+        double finalprice = ( baseprice + (baseprice * taxrate));
+
+        System.out.println(finalprice);
+
+
+
+
+    }
+
+
+
+}
---
+++ src/Task18092024/narrowingexample.java
@@ -0,0 +1,21 @@
+package Task18092024;
+
+public class narrowingexample {
+
+    public static void main(String[] args) {
+
+        double sensexreader = 12345.6789;
+
+        float narrowreading =(float) sensexreader;
+
+        System.out.println(sensexreader);
+
+        System.out.println(narrowreading);
+
+
+
+
+
+
+    }
+}
