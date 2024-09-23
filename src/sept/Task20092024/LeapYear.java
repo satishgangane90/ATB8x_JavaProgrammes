606a5e1c11cc199013872f0b2a1d687f1dc9b42e
satishgangane90 <satishgangane@gmail.com>
2024-09-23 15:47:11 +0530

    Task20092024

---
+++ src/sept/Task20092024/LeapYear.java
@@ -0,0 +1,22 @@
+package sept.Task20092024;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+
+
+    int year = 2033;
+
+    if(year%4 ==0){
+       System.out.println("The year is Leap year") ;
+    }
+    else{
+
+        System.out.println("The year is not Leap year");
+
+    }
+
+    }
+
+}
